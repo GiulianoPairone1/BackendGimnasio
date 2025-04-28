995525b3a3642d456c296c8997ab674ee2494edb
marianocastellielias <marianocastelli12@gmail.com>
2025-04-28 18:15:53 -0300

    Implement Repository Get Methods

--- GimnasioApi/Infrastructure/Data/ClientRepository.cs
+++ GimnasioApi/Infrastructure/Data/ClientRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Entities;
 using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,20 @@ namespace Infrastructure.Data
     public class ClientRepository : EfRepository<Client>, IClientRepository
     {
         public ClientRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<List<GymSession>> GetGymSessionAvaiableAsync()
+        {
+            return await _applicationDbContext.GymSessions
+                .Where(gymSession => gymSession.IsAvailable == true)
+                .ToListAsync();
+        }
+
+        public async Task<List<GymSession>> GetMyGymSessionsAsync(int userId)
+        {
+            return await _applicationDbContext.GymSessions
+                .Where(session => session.Clients.Any(c => c.Id == userId))
+                .Where(session => session.IsAvailable == true)
+                .ToListAsync();
+        }
     }
 }
